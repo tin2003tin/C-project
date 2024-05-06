ffafdf0cd312bc5978b2aa98705d0338580168f2
tin2003tin <152775124+tin2003tin@users.noreply.github.com>
2024-05-06 17:58:43 +0700

    add more example

--- global/vector/test/example_2.c
+++ global/vector/test/example_2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#include "./global/vector/vector.h"
+#include "../vector.h"
 
 typedef struct Book
 {
@@ -34,16 +34,17 @@ int main()
     Book book1 = book_create("Fairy Tail", "Tin", 200, true);
     Book book2 = book_create("Waterfall", "Job", 100, false);
     Book book3 = book_create("Gramble Guide", "Mark", 500, true);
-    
+
     T_Vector *vector = T_VECTOR_NEW(Book);
     t_vector_push_back(vector, &book1);
     t_vector_push_back(vector, &book2);
     t_vector_push_back(vector, &book3);
 
-    for (int index = 0; index < t_vector_get_size(vector); index++) {
+    for (int index = 0; index < t_vector_get_size(vector); index++)
+    {
         book_display(&(T_VECTOR_GET_VALUE_AS(Book, vector, index)));
         printf("\n");
-    } 
+    }
 
     t_vector_destroy(&vector);
     return 0;
